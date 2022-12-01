a1682f8eb42f6f2161adcfc49a0a7c12b87af765
Drew Burton <drew.burton.gc@gmail.com>
2022-12-01 18:25:23 -0500

    cleaned up day 1 imports

--- day01/Main.java
+++ day01/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
