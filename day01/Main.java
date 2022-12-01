0618bc630404cd0e7cd085b4cf196f025c8dbb02
Drew Burton <drew.burton.gc@gmail.com>
2022-12-01 18:02:40 -0500

    finished day 1

---
+++ day01/Main.java
@@ -0,0 +1,26 @@
+package day01;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		File f = new File("day01/input.txt");
+		try (Scanner scanner = new Scanner(f)) {
+			List<Integer> sortedCalories = scanner.findAll("(?>\\d+\\n)+|\\d+")
+					.map(o -> o.group())
+					.map(o -> Arrays.stream(o.split("\n"))
+							.mapToInt(Integer::parseInt)
+							.sum())
+					.sorted((a, b) -> b.compareTo(a))
+					.toList();
+			System.out.println(sortedCalories.get(0) + sortedCalories.get(1) + sortedCalories.get(2));
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
